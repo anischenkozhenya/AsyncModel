5aee46806e1bf55f61353a1980f8675b4742365b
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-31 15:33:40 +0300

    +Task2fix

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+//Создайте консольное приложение, в котором организуйте асинхронный вызов метода.
+//Используя конструкцию BeginInvoke передайте в поток некоторую информацию (возможно, в
+//формате строки). Организуйте обработку переданных данных в callback методе.
 
 namespace Task2
 {
@@ -10,6 +15,48 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            var deleg = new Func<int, int>(Method);
+            var callback = new AsyncCallback(callbackMethod);
+            string str = "Дополнительная информация в виде строки";
+            //Передоваемый аргумент в Method, делегат AsyncCallback, Дополнительный обьект
+            deleg.BeginInvoke(25, callback, str);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Метод вызывающийся после окончания работы основного метода
+        /// </summary>
+        /// <param name="ar">Значение типа IAsyncResult</param>
+        private static void callbackMethod(IAsyncResult ar)
+        {
+            Console.WriteLine("Колбек метод начался..");
+            Console.WriteLine("Id Потока Колбек метода: " + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Фоновый поток :"+Thread.CurrentThread.IsBackground);
+            //IAsyncResult приводим  к AsyncResult
+            var async = ar as AsyncResult;
+            //Получаем делегат
+            var del = (Func<int, int>)async.AsyncDelegate;
+            //Получаем результат
+            int result = del.EndInvoke(ar);
+            Console.WriteLine("Результат работы основного метода: "+result);
+            Console.WriteLine("Доролнительная инфа: "+ar.AsyncState);
+            Console.WriteLine("Колбек метод закончился");
+        }
+        /// <summary>
+        /// Принимает число и возвращает квадрат числа
+        /// </summary>
+        /// <param name="arg">Целочисленный аргумент</param>
+        /// <returns>Квадрат числа</returns>
+        private static int Method(int arg)
+        {
+            Console.WriteLine("Метод начало");
+            Console.WriteLine("Id Потока метода: "+Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Фоновый поток :"+Thread.CurrentThread.IsBackground);
+            int a = arg * arg;
+            Console.WriteLine("Метод окончен");
+            return a;//число 625
         }
     }
 }
+
