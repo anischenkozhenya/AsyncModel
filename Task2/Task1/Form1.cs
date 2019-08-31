a1e333d4b4e82e18e9fadef811ffca29efbb3432
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-31 16:48:38 +0300

    +task1 comment

--- Task2/Task1/Form1.cs
+++ Task2/Task1/Form1.cs
@@ -1,35 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Task1
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        /// <summary>
+        /// Делегат с сигнатурой как у метода Concat
+        /// </summary>
+        readonly Func<string, string, string,string> mydel = new Func<string, string, string,string>(Concat);
+        /// <summary>
+        /// Объект синхронизации
+        /// </summary>
+        SynchronizationContext sync;
+        /// <summary>
+        /// Метод конкатенации двух строк
+        /// </summary>
+        /// <param name="a">Строка из TextBox1</param>
+        /// <param name="b">Строка из TextBox2</param>
+        /// <param name="c">Строка метода вызова</param>
+        /// <returns>Конкатенацию двух строк типа String</returns>
+        static string Concat(string a, string b,string c)
         {
-            InitializeComponent();
+            MessageBox.Show("Поток выполнения:"+Thread.CurrentThread.ManagedThreadId.ToString()+"\nМетод выполнения: "+c);
+            return a + b;
         }
+        public void callback(IAsyncResult asyncResult)
+        {
+            MessageBox.Show(Thread.CurrentThread.ManagedThreadId.ToString());
+            AsyncResult async = (AsyncResult)asyncResult;
+            var delegateAdd = (Func<string, string, string>)async.AsyncDelegate;
+            var result = delegateAdd.EndInvoke(asyncResult);
+            sync.Post(delegate { textBox3.Text = result.ToString(); }, null);
+        }
+        
 
         private void BtnComplete_Click(object sender, EventArgs e)
-        {
-
+        {            
+            var res = mydel.BeginInvoke(textBox1.Text, textBox2.Text, "IsComplete", null, null);
+            while (true)
+            {
+                if (res.IsCompleted)
+                {
+                    break;
+                }
+            }
+            textBox3.Text = mydel.EndInvoke(res);
         }
 
         private void BtnEndInvoke_Click(object sender, EventArgs e)
         {
-
+            var result = mydel.BeginInvoke(textBox1.Text, textBox2.Text, "EndInvoke", null, null);
+            textBox3.Text = mydel.EndInvoke(result);
         }
 
         private void BtnCallback_Click(object sender, EventArgs e)
         {
-
+            sync = SynchronizationContext.Current;
+            var re = mydel.BeginInvoke(textBox1.Text, textBox2.Text, "Callback", callback, textBox3);
+        }
+        public Form1()
+        {
+            InitializeComponent();
         }
     }
 }
